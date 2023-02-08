81ca5862ac07ea1e2d1b6849103447a9c0621498
roodhouse <rughjm@gmail.com>
2023-02-08 15:02:45 -0600

    create constants for UI

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-// Define UI Vars (create vars for elements needed)
+// Define UI Vars (create vars for elements needed) -- done
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+
 
 
 // Load all event listeners
 loadEventListeners();
 
-// Load all event listeners (init all the functions)
+// Load all event listeners (init all the functions) -- here 
 function loadEventListeners(){
     
 }
