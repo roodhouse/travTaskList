4de91910336ee6a3c7b263201a7a659fe5420283
roodhouse <rughjm@gmail.com>
2023-02-08 14:53:05 -0600

    saved original, cleared out app.js to attempt on my own

--- app.js
+++ app.js
@@ -1,85 +1,50 @@
-// Define UI Vars
-const form = document.querySelector('#task-form');
-const taskList = document.querySelector('.collection');
-const clearBtn = document.querySelector('.clear-tasks');
-const filter = document.querySelector('#filter');
-const taskInput = document.querySelector('#task');
+// Define UI Vars (create vars for elements needed)
+
 
 // Load all event listeners
 loadEventListeners();
 
-// Load all event listeners
+// Load all event listeners (init all the functions)
 function loadEventListeners(){
-    // Add task event
-    form.addEventListener('submit', addTask);
-    // Remove task event
-    taskList.addEventListener('click', removeTask);
-    // Clear tasks
-    clearBtn.addEventListener('click', clearTasks);
-    // Filter tasks
-    filter.addEventListener('keyup', filterTasks);
+    
+}
+
+// Get Tasks from local storage
+function getTasks() {
+    
 }
 
 // Add task
 function addTask(e) {
-    if(taskInput.value === '' ) {
-        alert('Add a task')
-    }
+   
+}
 
-    // Create li element
-    const li = document.createElement('li');
-    // Add class
-    li.className = 'collection-item';
-    // Create text node and append to li
-    li.appendChild(document.createTextNode(taskInput.value));
-    // Create new link element
-    const link = document.createElement('a');
-    // Add class
-    link.className = 'delete-item secondary-content';
-    // Add icon html
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-    // Append link to li
-    li.appendChild(link);
-
-    // Append li to ul
-    taskList.appendChild(li);
-
-    // Clear input
-    taskInput.value = '';
-
-    e.preventDefault();
+// Store Task
+function storeTaskInLocalStorage(task) {
+    
 }
 
 // Remove Task
 function removeTask(e) {
-    if(e.target.parentElement.classList.contains('delete-item')){
-        if(confirm('Are you sure?')) {
+    
+}
 
-            e.target.parentElement.parentElement.remove();
-        }
-    }
+// Remove from LS
+function removeTaskFromLocalStorage(taskItem) {
+   
 }
 
 // Clear Tasks
 function clearTasks() {
-    // taskList.innerHTML = '';
-    // Faster
-    while(taskList.firstChild) {
-        taskList.removeChild(taskList.firstChild);
-    }
+   
+}
+
+// Clear Tasks from LS
+function clearTasksFromLocalStorage(){
+    
 }
 
 // Filter tasks
 function filterTasks(e) {
-    const text = e.target.value.toLowerCase();
-
-    document.querySelectorAll('.collection-item').forEach
-    (function(task){
-        const item = task.firstChild.textContent;
-        if(item.toLowerCase().indexOf(text) != -1) {
-            task.style.display = 'block'; 
-        } else {
-            task.style.display = 'none';
-        }
-    });
+    
 }
